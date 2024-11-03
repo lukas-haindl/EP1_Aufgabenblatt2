59815ce37f49bdd55576cc91a2958b96b899b59a
lukas-haindl <lukas.haindl03@icloud.com>
2024-11-03 21:15:49 +0100

    implement Aufgabe1

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -5,10 +5,47 @@
 import codedraw.CodeDraw;
 import codedraw.Palette;
 
+import java.awt.*;
+
 public class Aufgabe1 {
 
     public static void main(String[] args) {
 
-        //TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+        int ws = 400;
+        CodeDraw myDrawObj = new CodeDraw(ws, ws);
+        double radius = 1f/60f * ws;
+        double x = 2 * radius;
+        double y = 2 * radius;
+        int numbOfCircles = 15;
+        int numbOfSquares = 7;
+        for (int i = 0; i < numbOfCircles; i++) {
+            for (int j = 0; j < numbOfCircles; j++) {
+                myDrawObj.setColor(Color.BLACK);
+                myDrawObj.fillCircle(x, y, radius);
+                myDrawObj.setColor(Color.GRAY);
+                myDrawObj.setLineWidth(3);
+                myDrawObj.drawCircle(x, y, radius);
+                x += radius * 4;
+            }
+            x = 2 * radius;
+            y += radius * 4;
+        }
+
+        myDrawObj.setColor(Color.WHITE);
+        myDrawObj.fillSquare(1f/4 * ws + radius, 1f/4 * ws + radius, 1f/2 * ws - 2 * radius);
+        myDrawObj.setColor(Color.BLACK);
+        myDrawObj.setLineWidth(1);
+        myDrawObj.drawSquare(1f/4 * ws + radius, 1f/4 * ws + radius, 1f/2 * ws - 2 * radius);
+        y = 1f/4 * ws + radius * 2;
+
+        for (int i = 0; i < numbOfSquares; i++) {
+            x = 1f/4 * ws + radius * 2;
+            for (int j = 0; j < numbOfSquares; j++) {
+                myDrawObj.drawSquare(x, y, radius * 2);
+                x += radius * 4;
+            }
+            y += radius * 4;
+        }
+        myDrawObj.show();
     }
 }
